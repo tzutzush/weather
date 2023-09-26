c4fda756803b4444fcdd9759ab46570282a0c055
tzutzush <116487209+tzutzush@users.noreply.github.com>
2023-09-26 16:14:48 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.ts", "./**/*.html"],
+  content: ["./*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        sbar: "0 0px 8px -1px rgb(0 0 0 / 0.2), 0 0px 8px -2px rgb(0 0 0 / 0.2)",
+      },
+      colors: {
+        beige: "#f5f5f5",
+        lightGrey: "#eaecef",
+      },
+    },
   },
   plugins: [],
 };
