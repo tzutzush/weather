25f1a3fc53388c66c153bb58c75273df9e3f9d87
tzutzush <116487209+tzutzush@users.noreply.github.com>
2023-10-04 15:03:27 +0200

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,29 @@ module.exports = {
         beige: "#f5f5f5",
         lightGrey: "#eaecef",
       },
+      width: {
+        896: "896px",
+        740: "740px",
+        610: "610px",
+      },
+      screens: {
+        phone: { min: "375px", max: "428px" },
+
+        sm: { min: "640px", max: "767px" },
+        // => @media (min-width: 640px and max-width: 767px) { ... }
+
+        md: { min: "768px", max: "1023px" },
+        // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+        lg: { min: "1024px", max: "1279px" },
+        // => @media (min-width: 1024px and max-width: 1279px) { ... }
+
+        xl: { min: "1280px", max: "1535px" },
+        // => @media (min-width: 1280px and max-width: 1535px) { ... }
+
+        "2xl": { min: "1536px" },
+        // => @media (min-width: 1536px) { ... }
+      },
     },
   },
   plugins: [],
